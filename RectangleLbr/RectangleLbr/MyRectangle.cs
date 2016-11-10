5acc7b3dc7d720a604eda5088bc1aeae6b31b0df
egor43 <egor43.rus@mail.ru>
2016-11-10 18:44:15 +0300

    Дописана документация к библиотеке

--- RectangleLbr/RectangleLbr/MyRectangle.cs
+++ RectangleLbr/RectangleLbr/MyRectangle.cs
@@ -73,32 +73,59 @@ namespace RectangleLbr
 
         #region Методы
 
+        /// <summary>
+        /// Вычисляет площадь прямоугольника.
+        /// Может быть переопределен.
+        /// </summary>
+        /// <returns>площадь прямоугольника</returns>
         public virtual double GetArea()
         {
             return Width * Length;
         }
 
+        /// <summary>
+        /// Вычисляет периметр прямоугольника.
+        /// Может быть переопределен.
+        /// </summary>
+        /// <returns>периметр прямоугольника</returns>
         public virtual double GetPerimeter()
         {
-            if ((Width == 0) || (Length == 0))
-            {
-                if (Width == 0) return Length;
-                return Width;
+            if ((Width == 0) || (Length == 0))  // 
+            {                                   // Проверка на то, не является ли прямоугольник линией
+                if (Width == 0) return Length;  //
+                return Width;                   //
             }
             return 2 * (Width + Length);
         }
 
+        /// <summary>
+        /// Возвращает название фигуры.
+        /// Может быть переопределен.
+        /// </summary>
+        /// <returns>название фигуры</returns>
         public virtual string About()
         {
             return "Rectangle";
         }
 
+        /// <summary>
+        /// Переопределение оператора <
+        /// </summary>
+        /// <param name="object1">первый объект для сравнения</param>
+        /// <param name="object2">второй объект для сравнения</param>
+        /// <returns>если первый объект меньше второго - true, иначе - false</returns>
         public static bool operator <(MyRectangle object1, MyRectangle object2)
         {
             if (object1.GetArea() < object2.GetArea()) return true;
             return false;
         }
 
+        /// <summary>
+        /// Переопределение оператора >
+        /// </summary>
+        /// <param name="object1">первый объект для сравнения</param>
+        /// <param name="object2">второй объект для сравнения</param>
+        /// <returns>если первый объект больше второго - true, иначе - false</returns>
         public static bool operator >(MyRectangle object1, MyRectangle object2)
         {
             if (object1.GetArea() > object2.GetArea()) return true;
@@ -122,6 +149,9 @@ namespace RectangleLbr
 
         #region Свойства
 
+        /// <summary>
+        /// Высота параллелепипеда
+        /// </summary>
         public double Height
         {
             get
@@ -131,7 +161,7 @@ namespace RectangleLbr
 
             set
             {
-                if (value < 0) throw new ArgumentOutOfRangeException("Отрицательное значение недопустимо");
+                if (value < 0) throw new ArgumentOutOfRangeException("Отрицательное значение недопустимо"); //Проверка на корректность введенных данность
                 height = value;
             }
         }
@@ -140,6 +170,12 @@ namespace RectangleLbr
 
         #region Конструкторы
 
+        /// <summary>
+        /// Инициализирует новый экземпляр класса с указанными значениями
+        /// </summary>
+        /// <param name="width">Ширина</param>
+        /// <param name="length">Длина</param>
+        /// <param name="height">Высота</param>
         public MyVolumeRectangle(double width, double length, double height)
             : base(width, length)
         {
@@ -150,42 +186,71 @@ namespace RectangleLbr
 
         #region Методы
 
+        /// <summary>
+        /// Вычисляет площадь параллелепипеда.
+        /// </summary>
+        /// <returns>площадь параллелепипеда</returns>
         public override double GetArea()
         {
             return 2 * (Width * Height + Length * Height + Length * Width);
         }
 
+        /// <summary>
+        /// Вычисляет периметр параллелепипеда.
+        /// </summary>
+        /// <returns>периметр параллелепипеда</returns>
         public override double GetPerimeter()
         {
-            if ((Width == 0) || (Length == 0) || (Height == 0))
-            {
-                if (Height == 0) return base.GetPerimeter();
-                if (Width == 0)
-                {
-                    if (Length == 0) return Height;
-                    return 2 * (Length + Height); ;
+            if ((Width == 0) || (Length == 0) || (Height == 0))  //
+            {                                                    //
+                if (Height == 0) return base.GetPerimeter();     //
+                if (Width == 0)                                  // Проверка на то, не является ли параллелепипед прямоугольником или линией
+                {                                                //
+                    if (Length == 0) return Height;              //  
+                    return 2 * (Length + Height); ;              //
                 }
                 return 2 * (Width + Height); ;
             }
             return 4 * (Width + Length + Height);
         }
 
-        public double GetVolume()
+        /// <summary>
+        /// Вычисляет объем параллелепипеда.
+        /// Может быть переопределен.
+        /// </summary>
+        /// <returns>объем параллелепипеда</returns>
+        public virtual double GetVolume()
         {
             return Width * Length * Height;
         }
 
+        /// <summary>
+        /// Возвращает название фигуры.
+        /// </summary>
+        /// <returns>название фигуры</returns>
         public override string About()
         {
             return "VolumeRectangle";
         }
 
+        /// <summary>
+        /// Переопределение оператора <
+        /// </summary>
+        /// <param name="object1">первый объект для сравнения</param>
+        /// <param name="object2">второй объект для сравнения</param>
+        /// <returns>если первый объект меньше второго - true, иначе - false</returns>
         public static bool operator <(MyVolumeRectangle object1, MyVolumeRectangle object2)
         {
             if (object1.GetVolume() < object2.GetVolume()) return true;
             return false;
         }
 
+        /// <summary>
+        /// Переопределение оператора >
+        /// </summary>
+        /// <param name="object1">первый объект для сравнения</param>
+        /// <param name="object2">второй объект для сравнения</param>
+        /// <returns>если первый объект больше второго - true, иначе - false</returns>
         public static bool operator >(MyVolumeRectangle object1, MyVolumeRectangle object2)
         {
             if (object1.GetVolume() > object2.GetVolume()) return true;
